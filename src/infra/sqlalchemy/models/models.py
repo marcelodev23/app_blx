bd35870a337d7c52b69fc4771b8fcbbe3bb3df77
Marcelo Alves <130853760+marcelodev23@users.noreply.github.com>
2023-12-10 23:19:24 -0300

    Add files via upload

---
+++ src/infra/sqlalchemy/models/models.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String,Float
+from src.infra.sqlalchemy.config.database import Base
+
+
+
+class Produto(Base):
+    __tablename__  = "produto"
+    id = Column(Integer,primary_key=True,index=True )
+    nome = Column(String)
+    detalhes = Column(String)
+    disponivel = Column(Boolean)
+    preco=Column(Float)
+
+
+class Usuario(Base):
+    __tablename__ = 'usuario'
+    id = Column(Integer,primary_key=True,index=True)
+    nome = Column(String)
+    telefone = Column(String)
